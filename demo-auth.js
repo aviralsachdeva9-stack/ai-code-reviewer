ec947a0be15a2d2eac912288e2513bcc87e13d33
Aviral Sachdeva <aviralsachdeva9@gmail.com>
2026-07-06 15:40:04 +0530

    feat: added user login function

---
+++ demo-auth.js
@@ -0,0 +1,11 @@
+// demo-auth.js
+function loginUser(email, password) {
+    // 🚩 Bug 1: Hardcoded sensitive information (Secret Key)
+    const adminToken = "SUPER_SECRET_ADMIN_TOKEN_12345";
+    
+    // 🚩 Bug 2: SQL Injection vulnerability (Direct string concatenation)
+    let query = "SELECT * FROM users WHERE email = '" + email + "' AND password = '" + password + "'";
+    
+    console.log("Using token: ", adminToken);
+    return database.execute(query);
+}
